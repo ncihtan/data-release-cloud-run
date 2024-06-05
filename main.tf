b9b62ee4dbef821aaf43afec62874029822cb163
Adam Taylor <adam.taylor@sagebase.org>
2024-06-05 11:24:10 +0100

    Revert attempt to not create SAs

--- main.tf
+++ main.tf
@@ -32,10 +32,7 @@ resource "google_cloud_run_v2_job" "default" {
             }
         }
         timeout = "2700s"
-        service_account = coalesce(
-          length(google_service_account.sa) > 0 ? google_service_account.sa[0].email : null,
-          data.google_service_account.existing_sa.email
-        )
+        service_account = "${google_service_account.sa.email}"
     }
   }
   lifecycle {
